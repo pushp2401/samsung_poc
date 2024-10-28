f6b52a04c564dc6afc02e911314af6592790c848
pushp2401 <pushp.kumari@doaz.ai>
2024-10-28 10:48:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -133,17 +133,20 @@ def highlight_similar_sentences(resp , chunk , count , org_resp) :
             #         pass
             #     else :
             #         chunk_output += f"{line2}<br>"
-            if sim >= 0.8 :
-                ref_index = f"[{count}]" 
-                end_index_line1 = resp_copy.index(line1) + len(line1) #1 index increase for string slicing 
-                # resp_copy = resp_copy[ : end_index_line1] + ref_index + resp_copy[  end_index_line1 : ] 
-                resp_copy = resp_copy[:end_index_line1] + f"<span style='background-color:white;color:red'>{ref_index}</span>" + resp_copy[  end_index_line1 : ]
-                
-                start_index_line2 = chunk_copy.index(line2)
-                end_index_line2 = chunk_copy.index(line2) + len(line2) #1 index increase for string slicing 
-                # chunk_copy = chunk_copy[ : end_index_line2] + ref_index + chunk_copy[  end_index_line2 : ] 
-                chunk_copy = chunk_copy[ : start_index_line2] + f"<span style='background-color: DarkCyan;'>{line2}</span><span style='background-color: red;'>{ref_index}</span>" + chunk_copy[  end_index_line2 : ]
-                count += 1
+            if sim >= 0.85 :
+                try : 
+                    ref_index = f"[{count}]" 
+                    end_index_line1 = resp_copy.index(line1) + len(line1) #1 index increase for string slicing 
+                    # resp_copy = resp_copy[ : end_index_line1] + ref_index + resp_copy[  end_index_line1 : ] 
+                    resp_copy = resp_copy[:end_index_line1] + f"<span style='background-color:white;color:red'>{ref_index}</span>" + resp_copy[  end_index_line1 : ]
+                    
+                    start_index_line2 = chunk_copy.index(line2)
+                    end_index_line2 = chunk_copy.index(line2) + len(line2) #1 index increase for string slicing 
+                    # chunk_copy = chunk_copy[ : end_index_line2] + ref_index + chunk_copy[  end_index_line2 : ] 
+                    chunk_copy = chunk_copy[ : start_index_line2] + f"<span style='background-color: DarkCyan;'>{line2}</span><span style='background-color: red;'>{ref_index}</span>" + chunk_copy[  end_index_line2 : ]
+                    count += 1
+                except : 
+                    pass
     return resp_copy , chunk_copy , count
 
 
